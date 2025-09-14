317e990ed206b854cc101cf07d3bcb9a8fbf4bde
ysq <sgxiang1992@icloud.com>
2025-09-14 23:26:21 +0800

    feat: upgrade to v0.4.10

--- src/streaming/event_parser/protocols/pumpfun/parser.rs
+++ src/streaming/event_parser/protocols/pumpfun/parser.rs
@@ -105,16 +105,34 @@ fn parse_create_token_instruction(
         return None;
     }
     let mut offset = 0;
+    if offset + 4 > data.len() {
+        return None;
+    }
     let name_len = u32::from_le_bytes(data[offset..offset + 4].try_into().unwrap()) as usize;
     offset += 4;
+    if offset + name_len > data.len() {
+        return None;
+    }
     let name = String::from_utf8_lossy(&data[offset..offset + name_len]);
     offset += name_len;
+    if offset + 4 > data.len() {
+        return None;
+    }
     let symbol_len = u32::from_le_bytes(data[offset..offset + 4].try_into().unwrap()) as usize;
     offset += 4;
+    if offset + symbol_len > data.len() {
+        return None;
+    }
     let symbol = String::from_utf8_lossy(&data[offset..offset + symbol_len]);
     offset += symbol_len;
+    if offset + 4 > data.len() {
+        return None;
+    }
     let uri_len = u32::from_le_bytes(data[offset..offset + 4].try_into().unwrap()) as usize;
     offset += 4;
+    if offset + uri_len > data.len() {
+        return None;
+    }
     let uri = String::from_utf8_lossy(&data[offset..offset + uri_len]);
     offset += uri_len;
     let creator = if offset + 32 <= data.len() {
